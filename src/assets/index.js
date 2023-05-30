a19e716b10082131618e66408f7bcb0baa30c5d7
Aung-Zin-Latt <aungzinlatt007@gmail.com>
2023-05-30 11:16:33 +0630

    First Commit

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import logo from "./logo.svg";
+import logo1 from "./aungzinlatlogo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import vuejs from "./tech/vuejs.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import php from "./tech/php.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import laravel from "./tech/laravel.svg";
+import livewire from "./tech/livewire.png"
+import jquery from "./tech/jquery.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import bootstrap from "./tech/bootstrap-5.svg";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import react from "./react.svg";
+
+export {
+  logo,
+  logo1,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  vuejs,
+  figma,
+  git,
+  php,
+  html,
+  javascript,
+  laravel,
+  livewire,
+  jquery,
+  reactjs,
+  redux,
+  tailwind,
+  bootstrap,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  react,
+};
